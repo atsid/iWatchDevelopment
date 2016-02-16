693d74326cc132a6b677ae1a104d23e95986c631
Gregory Kedge <gregwork@kedges.com>
2016-02-15 19:29:10 -0500

    Initial commit

---
+++ FlightWatch WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  FlightWatch WatchKit Extension
+//
+//  Created by Greg Kedge on 2/15/16.
+//  Copyright © 2016 ATS Inc. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
