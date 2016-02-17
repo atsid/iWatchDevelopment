e56538499ccbc74de3d292705435b8143a27c94e
Gregory Kedge <gregwork@kedges.com>
2016-02-16 20:51:16 -0500

    Added changes to establish communications with watch for the purposes of sending it data.

--- FlightWatch/ViewController.swift
+++ FlightWatch/ViewController.swift
@@ -7,12 +7,29 @@
 //
 
 import UIKit
+import WatchConnectivity
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, WCSessionDelegate {
+    var watchSession : WCSession?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        /*
+        * If this device can support a WatchConnectivity session,
+        * obtain a session and activate.
+        *
+        * It isn't usually recommended to put this in viewDidLoad,
+        * we're only doing it here to keep the app simple
+        *
+        * Note: Even though we won't be receiving messages in the View Controller,
+        * we still need to supply a delegate to activate the session
+        */
+        if(WCSession.isSupported()){
+            watchSession = WCSession.defaultSession()
+            watchSession!.delegate = self
+            watchSession!.activateSession()
+        }
     }
 
     override func didReceiveMemoryWarning() {
