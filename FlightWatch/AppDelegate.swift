1a6a14ca5a6f5c486b869f08d9b37c50b3321d04
Greg Kedge <greg.kedge@atsid.com>
2016-02-19 13:41:45 -0500

    Added Flight data JSON and prepared to add QRCode lib.

--- FlightWatch/AppDelegate.swift
+++ FlightWatch/AppDelegate.swift
@@ -65,7 +65,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, WCSessionDelegate {
     func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
         if (WCSession.isSupported()){
-//            var flight: [String:AnyObject] = [:]
             
             let asset = NSDataAsset(name: "Flights", bundle: NSBundle.mainBundle())
             do {
